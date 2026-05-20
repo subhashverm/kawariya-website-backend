ff91877e43792b1c04945b2c7ec10e9988b8035d
subhash <subhash27042004@gmail.com>
2026-05-20 20:42:46 +0530

    dotenv update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+console.log(process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
